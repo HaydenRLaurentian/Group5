4fae75490a955264779f028ab32f07a18fde0da9
Hayden <hross1@laurentian.ca>
2022-04-11 21:20:21 -0400

    Commited

--- userprog/UserProcess.java
+++ userprog/UserProcess.java
@@ -2,9 +2,9 @@ package nachos.userprog;
 
 import nachos.machine.*;
 import nachos.threads.*;
-import nachos.userprog.*;
 
 import java.io.EOFException;
+import java.util.LinkedList;
 
 /**
  * Encapsulates the state of a user process that is not contained in its
@@ -153,6 +153,7 @@ public class UserProcess {
      * @return	the number of bytes successfully transferred.
      */
     public int writeVirtualMemory(int vaddr, byte[] data) {
+    	
 	return writeVirtualMemory(vaddr, data, 0, data.length);
     }
 
@@ -287,7 +288,8 @@ public class UserProcess {
 	    Lib.debug(dbgProcess, "\tinsufficient physical memory");
 	    return false;
 	}
-
+	lock.acquire();//??????????????????????????????????????????????????????????
+	
 	// load sections
 	for (int s=0; s<coff.getNumSections(); s++) {
 	    CoffSection section = coff.getSection(s);
@@ -346,7 +348,7 @@ public class UserProcess {
 	return 0;
     }
 
-
+//PHASE 2---------------------------------------------------------------------------
     private static final int
         syscallHalt = 0,
 	syscallExit = 1,
@@ -391,15 +393,489 @@ public class UserProcess {
 	switch (syscall) {
 	case syscallHalt:
 	    return handleHalt();
-
-
+	case syscallExit:
+		handleExit(a0);
+		break;
+	case syscallExec:
+		return handleExec(a0,a1,a2);
+	case syscallJoin:
+		return handleJoin(a0,a1);
+	case syscallCreate:
+		return handleCreate(a0);
+	case syscallOpen:
+		return handleOpen(a0);
+	case syscallRead:
+		return handleRead(a0,a1,a2);
+	case syscallWrite:
+		return handleWrite(a0,a1,a2);
+	case syscallClose:
+		return handleClose(a0);
+	case syscallUnlink:
+		return handleUnlink(a0);
 	default:
 	    Lib.debug(dbgProcess, "Unknown syscall " + syscall);
 	    Lib.assertNotReached("Unknown system call!");
 	}
 	return 0;
     }
+    
+    
+    /**
+     * handleOpen
+     * 
+     * Handle open(int *fileAddress) sys call
+     * check for invalid address, attempt to read fileName
+     * find available file slot 1-16 if none of these return an error
+     * open file.  if there's an error opening the file or the file 
+     * is null return error
+     * 
+     * @param fileAddress the address of the file to open
+     * @return an int value either -1 if an error occurred or index value  
+     *         representing the file slot # within the fileDescriptorTable
+     */
+    
+    private int handleOpen(int fileAddress){
+    	
+    	if (fileAddress <= 0){
+    		Lib.debug(dbgProcess, "handleOpen: Invalid address");
+    		return -1;
+    	}
+    	
+    	String fileName = readVirtualMemoryString(fileAddress, 256);
+    	
+    	if (fileName == null){
+    		Lib.debug(dbgProcess,  "handleOpen: Illegal Filename");
+    		return -1;
+    	}
+    	
+    	//check for available file slot 1-16
+    	
+    	int emptyIndex = -1;
+    	
+    	for(int i=2; i < 16 || emptyIndex > 0; i++){  
+    		if(fileDescriptorTable[i] == null){
+    			emptyIndex = i;
+    		}
+    	}
+    	if (emptyIndex == -1){
+    		Lib.debug(dbgProcess, "handleOpen: No available file slot");
+    		return -1;
+    	}
+    	
+    	
+    	//open file: if error show at which index
+    	try{
+    		OpenFile fileToOpen = ThreadedKernel.fileSystem.open(fileName, false);
+    		//or use:Filesystem fs = Machine.stubFileSystem(); 
+    		//       OpenFile file = fs.open(fileName, createOnOpen); ????????????????????????
 
+    		if (fileToOpen == null){
+    			Lib.debug(dbgProcess, "handleOpen: could not open file at index: " + emptyIndex);
+    			return - 1;
+    		}
+    		else{
+    			fileDescriptorTable[emptyIndex] = fileToOpen;
+    			return emptyIndex; 
+    		}
+    		
+    	}catch (Exception ex){
+    		Lib.debug(dbgProcess, "handleOpen: index: " + emptyIndex + " exception: " + ex);
+    		return -1;
+    	}
+    }
+    
+    
+    /**
+     * handleCreate
+     * 
+     * handle create() sys call
+     * creates the file virtAddr. if the file exists open it
+     * @param virtAddr the filename of the file to be created
+     * @return int value of fileDescriptorTable index if file 
+     * 		   was created, -1 if there's an error
+     */
+    
+    private int handleCreate(int virtAddr){
+    	
+    	//check name validity
+    	if(virtAddr < 0){
+    		Lib.debug(dbgProcess, "handleCreate: invalid virtual address");
+    		return -1;    		
+    	}
+    	
+    	String fileName = readVirtualMemoryString(virtAddr, 256);
+    	
+    	if (fileName == null){
+    		Lib.debug(dbgProcess, "handleCreate: Illegal Filename");
+    		return -1;
+    	}
+    	
+    	//check for available file slot 1-16
+    	
+    	int emptyIndex = -1;
+    	
+    	for(int i=2; i < 16 || emptyIndex > 0; i++){   
+    		if(fileDescriptorTable[i] == null){
+    			emptyIndex = i;
+    		}
+    	}
+    	if (emptyIndex == -1){
+    		Lib.debug(dbgProcess, "handleCreate: No available file slot");
+    		return -1;
+    	}  	
+
+    	//create file: if cannot create file, show error
+    	try{
+    		OpenFile fileToCreate = ThreadedKernel.fileSystem.open(fileName, true);
+    		//or use:Filesystem fs = Machine.stubFileSystem(); 
+    		//OpenFile file = fs.open(fileName, true); ????????????????????????
+
+    		if (fileToCreate == null){
+    			Lib.debug(dbgProcess, "handleCreate: could not create file");
+    			return - 1;
+    		}
+    		else{
+    			fileDescriptorTable[emptyIndex] = fileToCreate;
+    			return emptyIndex; 
+    		}
+    	}catch (Exception ex){
+    		Lib.debug(dbgProcess, "handleCreate: index: " + emptyIndex + " exception: " + ex);
+    		return -1;
+    	}    	
+    }
+    
+/**
+ *  handleClose
+ *  
+ *  get the file index from fileDescriptorTable
+ *  close the file & set fileDescriptorTable[index] to null
+ *  @param index the index of the file to be removed from fileDescriptorTable
+ *  @return status 0 if close was successful, -1 otherwise
+ */    
+    private int handleClose(int index){
+    	
+    	int status = 0;
+    	
+    	//ensure index is greater than 0 and less than 16
+    	//ensure fileDescriptorTable[index] isnt already null
+    try{
+    	if (index < 0 || index > 16 || fileDescriptorTable[index] == null){
+    		Lib.debug(dbgProcess, "handleClose: index out of bounds or already null");
+    		return -1;
+    	}
+    	else{
+    		fileDescriptorTable[index].close();
+    		fileDescriptorTable[index] = null;
+    		return status;
+    	}
+    }catch (Exception ex){
+    	Lib.debug(dbgProcess, "handleClose: could not close file: " + ex);
+    	return -1;
+    }
+    }
+    
+    /**
+     *  handleUnlink
+     *  
+     *  get fileName at address sent as parameter
+     *  if fileName is null return error else remove from ThreadedKernel.filesystem
+     *  @param nameAddr virtual memory address
+     *  @return int value 0 if remove was success, -1 otherwise  
+     */
+    
+    private int handleUnlink(int nameAddr){
+    	
+    	String fileName = readVirtualMemoryString(nameAddr, 256);
+    	
+    	if (fileName == null){
+    		Lib.debug(dbgProcess, "handleUnlink: no file to unlink");
+    		return -1;
+    	}else{
+    		boolean succeeded = ThreadedKernel.fileSystem.remove(fileName);
+    		
+    		if (!succeeded){
+    			return -1;
+    		}
+    		else {
+    			return 0;
+    		}
+    	}
+    }
+    
+    /**
+     *  handleRead
+     *  
+     *  check fileDescriptor value, create array to save data to, check if 
+     *  inputFile at fileDescript index is null.  if Not null read file
+     *  
+     *  @param fileDescript is the file descriptor from which to read
+     *  @param memAddr memory address to store data
+     *  @param numBytes number of bytes to write
+     *  @return -1 if error, fileDescript+data+offset+bytesRead to writeVirtualMemory
+     */
+    private int handleRead(int fileDescript, int memAddr, int numBytes){
+    	  
+    	if (fileDescript < 0 || fileDescript > 16){
+    		Lib.debug(dbgProcess, "handleRead: fileDescriptor out of bounds");
+    		return -1; 
+    	}
+    try{
+    	byte[] data = new byte[numBytes];
+    	
+    	OpenFile inputFile = fileDescriptorTable[fileDescript];
+    	
+    	if (inputFile == null){
+    		return -1;
+    	}
+    	int bytesRead = inputFile.read(data, 0, data.length);
+    	
+    	if (bytesRead <= 0){
+    		return -1;
+    	}
+    	//read data from memory 
+    	int bytesWritten = writeVirtualMemory(fileDescript, data, 0, bytesRead);
+    	return bytesWritten; 
+    	
+    }catch (Exception ex){
+    	Lib.debug(dbgProcess, "handleRead: could not read file: " + ex);
+    	return -1;
+    }
+    }
+    
+    /**
+     * handleWrite
+     * 
+     *  writes data bytes in numBytes located at memAddr
+     *  
+     *  @param fileDescript is the file descriptor from which to read
+     *  @param memAddr memory address to store data
+     *  @param numBytes number of bytes to write
+     *  @return -1 if error, number of bytes written otherwise 
+     */
+    private int handleWrite(int fileDescript, int memAddr, int numBytes){
+    	
+    	if (fileDescript < 0 || fileDescript > 16){
+    		Lib.debug(dbgProcess, "handleWrite: fileDescriptor out of bounds");
+    		return -1; 
+    	}
+    try {
+    	
+    	byte[] data = new byte[numBytes];
+    	
+    	int readBytes = readVirtualMemory(memAddr, data, 0, numBytes);
+    	
+    	if (readBytes != numBytes){
+    		Lib.debug(dbgProcess, "handleWrite: error reading bytes");
+    		return -1; 
+    	}
+    	
+    	OpenFile outputFile = fileDescriptorTable[fileDescript];
+    	
+    	if (outputFile == null){
+    		Lib.debug(dbgProcess, "handleWrite: no file at fileDescriptorTable index");
+    		return -1;
+    	}
+    	
+    	//write data to memory 
+    	int writtenBytes = outputFile.write(data, 0, data.length);
+    	
+    	if (writtenBytes <= data.length){
+    		Lib.debug(dbgProcess, "handleWrite: bytes written < total data bytes");
+    		return -1;
+    	}
+    	return writtenBytes;
+    	
+    }catch (Exception ex){
+    	Lib.debug(dbgProcess, "handleWrite: could not write file: " + ex);
+    	return -1;
+    }
+    }
+    
+    /**
+     *  handleExec
+     *  
+     *  Execute program from file with args in child process with unique process ID
+     *  checks virtual address for file and checks file for the .coff extension
+     *  get string file name and checks for null value, read contents of file increases
+     *  address variable to new address, creates child process and adds it to childProcessList
+     *  
+     *  @param fileNameVaddr the virtual address of the .coff file with executable instr
+     *  @param argNum number of arguments 
+     *  @param argOffset the virtual address offset
+     *  @return -1 if error, child processID otherwise
+     */
+    private int handleExec(int fileNameVaddr, int argNum, int argOffset){
+    
+    	//check if fileNameVaddr is less than 0
+    	if (fileNameVaddr < 0){
+    		Lib.debug(dbgProcess, "handleExec: invalid address");
+    		return -1; 
+    	}
+    	//Check string name for file
+    	String fileName = readVirtualMemoryString(fileNameVaddr, 256);
+    	
+    	if(fileName == null || !fileName.endsWith(".coff")) {
+    		Lib.debug(dbgProcess, "handleExec: error regarding the file name or extension");
+    		return -1;
+    	}
+    	
+    	String[] vMemArgs = new String[argNum];
+    	String vMemString;
+    	int currentVaddr = fileNameVaddr; 
+    	
+    	for(int i = 0; i < argNum; i++){
+    		byte[] data = new byte[4];
+    	
+    		int readBytes = readVirtualMemory(currentVaddr, data);
+    	
+    		if (readBytes != data.length){
+    			return -1;
+    		}
+    		int ptrToVaddr = Lib.bytesToInt(data, 0);
+    		
+    		if (ptrToVaddr == 0){
+    			Lib.debug(dbgProcess, "handleExec: error converting bytes to int");
+    			return -1;
+    		}else{
+    			vMemString = readVirtualMemoryString(ptrToVaddr, 256);
+    			
+    			if (vMemString == null){
+    				Lib.debug(dbgProcess, "handleExec: error reading memory string");
+    				return -1;
+    			}
+    		}
+    		vMemArgs[i] = vMemString;
+    		currentVaddr += 4;
+    	}
+    		UserProcess child = UserProcess.newUserProcess();
+    		if (child.execute(fileName,vMemArgs)){
+    			this.childProcessList.add(child);
+    			child.parent = this;
+    			return child.processID;
+    		}else{
+    			Lib.debug(dbgProcess, "handleExec: error executing the process");
+    			return -1;
+    		}
+    }
+    
+    /**
+     *  handleJoin
+     *  
+     *  ensure thread is NOT currentThread, get child process from childProcessList
+     *  if child processID from childProcessList is equal to processId sent as arg it 
+     *  becomes joinChild and removed from childProcessList then joined and written to 
+     *  memory
+     *  
+     *  @param processId the ID of the process to be joined
+     *  @param addr the address of the file to be executed
+     *  @return -1 if error, 1 if process is successful 
+     *  
+     */
+    private int handleJoin(int processId, int addr){
+    	
+    	if (this.processID == processId){
+    		Lib.debug(dbgProcess,  "handleJoin: cant join itself");
+    		return -1;
+    	}
+    	
+    	UserProcess joinChild = null;
+    	int index = -1;
+    	boolean gotProcess = false;
+    	
+    //lock.acquire();
+    boolean machineStatus = Machine.interrupt().disable();
+    
+    	for(int i = 0; i < childProcessList.size()|| gotProcess == true; i++) {
+    	
+    		if (this.childProcessList.get(i).processID == processId){
+    			joinChild = this.childProcessList.get(i);
+    			gotProcess = true;
+    			index++;
+    		}
+    	}
+    	gotProcess = false;
+    	
+    	//if childProcessList.get didnt work or didnt get anything
+    	if(joinChild == null || index == -1){
+    		Lib.debug(dbgProcess,  "handleJoin: error getting process from list ");
+    //lock.release();
+    Machine.interrupt().restore(machineStatus);
+    		return -1;
+    	}
+    	
+    	if( joinChild.processID == this.processID){
+    		joinChild.thread.join();
+    		Lib.debug(dbgProcess,  "handleJoin: joining thread: " + toString());
+    		joinedThreadIdList.add(joinChild.processID);
+    	}
+    	//remove joinChild from childProcessList
+    	this.childProcessList.remove(index);
+    	joinChild.parent = null;
+    	
+    	 //need to check state of process for null (if process is finished)...???????????????????
+    	//if(this.status == statusFinished)
+    	if (this.thread.schedulingState == null){
+    		
+    		byte[] data = new byte[4];
+    		int writtenBytes = writeVirtualMemory(addr, data);
+    		
+    		if (writtenBytes != 4){
+    			Lib.debug(dbgProcess,  "handleJoin: error writing bytes");
+    //lock.release();
+    Machine.interrupt().restore(machineStatus);			
+    			return -1;    			
+    		}else{
+    //lock.release();
+    			thread.sleep(); //???????????????????????????????????????????????
+    Machine.interrupt().restore(machineStatus);			
+    			return 1;
+    		}    		
+    	}else{
+    		Lib.debug(dbgProcess,  "handleJoin: error regarding thread state");
+    		return -1; 
+    	}
+    }
+
+    /**
+     * handleExit
+     * 
+     * terminates current process. Clear childProcessList and any
+     * parent processes, file descriptors and process ids associated
+     * 
+     * @param status status of process... why is it never used??????????????????????????
+     * 
+     */
+    private void handleExit(int status){
+    	
+    	if (parent != null){
+    		
+    		lock.acquire();
+    		
+    		for (int i = 0; i < childProcessList.size(); i++){
+    			handleClose(i);
+    			childProcessList.get(i).parent = null;
+    		}
+    		childProcessList.clear();
+    		
+    		lock.release();
+    		
+    		Lib.debug(dbgProcess,  "handleExit: removed child processes...now terminating");
+    		
+    		if (this.processID == 0) {
+    			Kernel.kernel.terminate();
+    		}else{
+    			KThread.finish();
+    		}  		
+    		Lib.debug(dbgProcess,  "handleExit: good bye");
+    	}
+    }
+    
+    
+    
+    
+    
+    
+    
     /**
      * Handle a user exception. Called by
      * <tt>UserKernel.exceptionHandler()</tt>. The
@@ -446,4 +922,24 @@ public class UserProcess {
 	
     private static final int pageSize = Processor.pageSize;
     private static final char dbgProcess = 'a';
+   
+//GLOBAL VARIABLES FOR PHASE 2........................
+    
+    // for handleOpen()
+    private OpenFile[] fileDescriptorTable;
+    
+    //for loadSections( )
+    private Lock lock = new Lock();
+    
+    //for handleExec( )
+    private LinkedList<UserProcess> childProcessList;
+    private UserProcess parent;
+    private int processID;
+    
+    //for handleJoin( )
+    private UThread thread;
+    private LinkedList<Integer> joinedThreadIdList;
+    
+    
+   
 }
